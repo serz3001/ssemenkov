fecaa5707c910732e82ad92f563ab3291b926254
Sergey Semenkov <sergey.semenkov.1993@gmail.com>
2018-02-26 13:22:05 +0300

    1.3. JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,14 +1,14 @@
-package ru.job4j;
+п»їpackage ru.job4j;
 
 /**
-	Class Класс Calculate
+	Class РљР»Р°СЃСЃ Calculate
 	@author ssemenkov
 	@since 25.02.2018
 	@version 1
 */
 public class Calculate {
 /**
-	Метод main. Выводит на жкран надпись "Hello, world!"
+	РњРµС‚РѕРґ main. Р’С‹РІРѕРґРёС‚ РЅР° Р¶РєСЂР°РЅ РЅР°РґРїРёСЃСЊ "Hello, world!"
 */
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
--- chapter_001/src/main/java/ru/job4j/package-info.java
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -1,4 +1,4 @@
-/**
+﻿/**
 * Package for calculate task.
 *
 * @author Sergey Semenkov (sergey.semenkov.1993@gmail.com)
