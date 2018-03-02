e8741104f2993e4f222c38264a05cd5efdc1e666
Sergey Semenkov <sergey.semenkov.1993@gmail.com>
2018-03-02 09:37:33 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,15 +1,26 @@
-﻿package ru.job4j;
+package ru.job4j;
 
 /**
-	Class Класс Calculate
-	@author ssemenkov
-	@since 25.02.2018
-	@version 1
+Calculate.
+@author ssemenkov
+@since 25.02.2018
+@version 1
 */
 public class Calculate {
-/**
-	Метод main. Выводит на жкран надпись "Hello, world!"
-*/
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+	    return "Echo, echo, echo : " + name;
+	}
+
+
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
 	}
--- chapter_001/src/test/java/ru/job4j/package-info.java
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -1,8 +1,8 @@
 /**
-* Package for calculate task.
+* Package for calculateTest task.
 *
 * @author Sergey Semenkov (sergey.semenkov.1993@gmail.com)
 * @version $Id$
-* @since 25/02/2018
+* @since 27/02/2018
 */
 package ru.job4j;
