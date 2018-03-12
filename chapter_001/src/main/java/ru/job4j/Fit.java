461987c549c84cb997aaa6d8ac418b944506c293
Sergey Semenkov <sergey.semenkov.1993@gmail.com>
2018-03-12 16:09:45 +0300

    001.3.3.3. Идеальный вес

--- chapter_001/src/main/java/ru/job4j/Fit.java
+++ chapter_001/src/main/java/ru/job4j/Fit.java
@@ -4,9 +4,9 @@ package ru.job4j;
  * Программа расчета идеального веса.
  */
 public class Fit {
-    private final int MEN_INDEX = 100;
-    private final int WOMEN_INDEX = 110;
-    private final double MULTIPLIER = 1.15;
+    private static final int MEN_INDEX = 100;
+    private static final int WOMEN_INDEX = 110;
+    private static final double MULTIPLIER = 1.15;
     /**
      * Идеальный вес для мужщины.
      * @param height Рост.
