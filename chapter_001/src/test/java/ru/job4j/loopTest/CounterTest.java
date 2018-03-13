d439b86587f96467e883d2e0521f5bb3918ac5bc
Sergey Semenkov <sergey.semenkov.1993@gmail.com>
2018-03-13 12:54:39 +0300

    001.4.1. Подсчёт чётных чисел в диапазоне[#192]

---
+++ chapter_001/src/test/java/ru/job4j/loopTest/CounterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.loopTest;
+
+import org.junit.Test;
+import ru.job4j.loop.Counter;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Sergey Semenkov
+ * @version $Id$
+ * @since 0.1
+ */
+public class CounterTest {
+    @Test
+    public void countTest(){
+        Counter count = new Counter();
+        int result = count.add(1, 10);
+        assertThat(result, is(30));
+
+    }
+}
