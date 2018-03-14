ebfe48997802383b5a692dc6257111634a567758
Sergey Semenkov <sergey.semenkov.1993@gmail.com>
2018-03-14 17:33:28 +0300

    001.4.5. Рефакторинг кода[#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Pyramid.java
+++ chapter_001/src/main/java/ru/job4j/loop/Pyramid.java
@@ -1,34 +1,37 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 public class Pyramid {
-    public String paintRight(int height){
-        // Буфер для результата.
-        StringBuilder screen = new StringBuilder();
-        // ширина будет равна высоте.
-        // внешний цикл двигается по строкам.
-        for (int row = 0; row < height; row++) {
-            // внутренний цикл определяет положение ячейки в строке.
-            for (int column = 0; column < height; column++) {
-                // если строка равна ячейке, то рисуем галку.
-                // в данном случае определяем, сколько галок будет на строке
-                if (row >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            // добавляем перевод строки.
-            screen.append(System.lineSeparator());
-        }
-        // Получаем результат.
-        return screen.toString();
+    public String paintRight(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
 
     public String paintLeft(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
+    public String paintPyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int width, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
         for (int row = 0; row < height; row++) {
-            for (int column = 0; column < height; column++) {
-                if (row >= height - column - 1) {
+            for (int column = 0; column < width; column++) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -39,21 +42,6 @@ public class Pyramid {
         return screen.toString();
     }
 
-    public String paintPyramid(int height) {
-        StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
-        for (int row = 0; row < height; row++) {
-            for (int column = 0; column < weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
-    }
     public static void main(String[] args) {
         Pyramid pyramid = new Pyramid();
         System.out.println(pyramid.paintPyramid(4));
