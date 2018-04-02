c5fd69a0bde650f36afc31563dc1c75905ca43c4
Sergey Semenkov <sergey.semenkov.1993@gmail.com>
2018-04-02 08:37:48 +0300

    001.5.6. Удаление дубликатов в массиве. [#225]

--- chapter_001/src/main/java/ru/job4j/arrays/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/arrays/ArrayDuplicate.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 
 public class ArrayDuplicate {
     public String[] remove(String[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
-                if (array[j].equals(array[j+1])) {
-                    array[j+1] = "00000";
+                if (array[j] != null && array[j].equals(array[j+1])) {
+                    array[j+1] = null;
                 }
             }
         }
         int count = 0; //подсчитывает количество пустых значений в массиве
         for (int i = 0; i < array.length-1; i++) {
             for (int j = 0; j < array.length-1; j++) {
-                if(array[j].equals("00000")) {
+                if(array[j] == null) {
                     array[j] = array[j + 1];
-                    array[j + 1] = "00000";
+                    array[j + 1] = null;
                     count++;
                 }
             }
